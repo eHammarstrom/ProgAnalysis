fa3ebbb809eadb3db4616a595bccc962c9fea21f
Hampus Balldin <hampus.balldin@gmail.com>
2018-09-28 15:55:42 +0200

    Begin own fix-point alg.

--- src/main/java/eda045f/exercises/flowgraph/AbstractFlowAnalysis.java
+++ src/main/java/eda045f/exercises/flowgraph/AbstractFlowAnalysis.java
@@ -5,31 +5,36 @@ import soot.toolkits.graph.InverseGraph;
 import soot.toolkits.scalar.ForwardFlowAnalysis;
 import soot.util.Switchable;
 
-public abstract class AbstractFlowAnalysis<V extends Switchable, N extends Switchable, T, A>
-		extends ForwardFlowAnalysis<N, A> {
+public abstract class AbstractFlowAnalysis<V extends Switchable, N extends Switchable, T, A> {
+		/* extends ForwardFlowAnalysis<N, A> { */
 	
 	protected DomainSet<T> abstractDomain;
+    private DirectedGraph<N> graph;
+
+    private HashMap<N, A> flowMap = new HashMap<N, A>();
+    
 
 	public AbstractFlowAnalysis(DirectedGraph<N> graph, DomainSet<T> abstractDomain, boolean forward) {
-		super(forward ? graph : new InverseGraph<>(graph));
 		this.abstractDomain = abstractDomain;
+        this.graph = forward ? graph : new InverseGraph<>(graph);
+
+
 	}
 
-    /* protected abstract void copy(A src, A dst); */
-    /* protected abstract A newInitialFlow(); */
-    /*  */
-    /* public A getFlowBefore(N inSet) { */
-    /*  */
-    /* } */
-    /*  */
-    /* public A getFlowAfter(N inSet) { */
-    /*  */
-    /* } */
-    /* public void flowThrough(A in, N node, A out); */
-    /* public void merge(A in1, A in2, A out); */
-    /*  */
-    /*  */
-    /* protected void doAnalysis() { */
-    /*  */
-    /* } */
+    protected abstract void copy(A src, A dst);
+    protected abstract A newInitialFlow();
+
+    public A getFlowBefore(N inSet) {
+
+    }
+
+    public A getFlowAfter(N inSet) {
+
+    }
+    public void flowThrough(A in, N node, A out);
+    public void merge(A in1, A in2, A out);
+
+
+    protected void doAnalysis() {
+    }
 }
